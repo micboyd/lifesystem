eb24b8e92032761614bc2dead73f486470509e06
Michael Boyd <michael_boyd@live.co.uk>
2026-09-22 08:55:49 +0100

    tabs width not full

--- client/src/pages/SavingsForecast.tsx
+++ client/src/pages/SavingsForecast.tsx
@@ -2205,6 +2205,7 @@ export default function SavingsForecast() {
                         tabs={['Projection', 'Savings to date', 'Target planner']}
                         value={view}
                         onChange={(t) => setView(t as typeof view)}
+                        className="self-start"
                     />
 
                     {view === 'Savings to date' ? (
